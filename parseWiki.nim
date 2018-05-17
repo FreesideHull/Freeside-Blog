57724d912cbb0e652a46b864632c5b6002f75f5a
Kieran Coldron <kieran@coldron.com>
2018-05-17 18:14:24 +0100

    Cleanup nim code

--- parseWiki.nim
+++ parseWiki.nim
@@ -1,32 +1,47 @@
 import re, strutils, json, httpclient, strutils, os
 
-var
-  client = newHttpClient()
-  forum_url = "https://forums.freeside.co.uk"
-  content = client.getContent(forum_url & "/c/wiki.json").parseJson
-  topics = content["topic_list"]["topics"]
-for topic in topics.items:
-  var id = topic["id"].getInt()
-  var raw_post = client.getContent(forum_url &  "/raw/" & intToStr(id))
-  var slug = topic["slug"].getStr()
-  var title = topic["title"].getStr()
-
-  var front_matter = """
+# The front matter is required for jekyll to recognise the file.
+let front_matter = """
 ---
 layout: page
 title: $1
-permalink: /wiki/$2.html
+permalink: /wiki/$2
 categories: [wiki]
 ---
 """
 
-  var page = front_matter.format(title, slug) & raw_post
-  if not existsDir("_pages"):
-        createDir("_pages")
-  writeFile("_pages/" & topic["slug"].getStr() & ".md", page)
-  if not existsDir("uploads/default/original/1X/"):
-        createDir("uploads/default/original/1X/")
+let forum_url = "https://forums.freeside.co.uk"
+
+#Make page folder
+if not existsDir("_pages"):
+  createDir("_pages")
+
+
+# Make image folder
+if not existsDir("uploads/default/original/1X/"):
+  createDir("uploads/default/original/1X/")
+
+
+# Get topics list
+var client = newHttpClient()
+let
+  content = client.getContent(forum_url & "/c/wiki.json").parseJson
+  topics = content["topic_list"]["topics"]
+
+# Loop over topics saving them to file
+for topic in topics.items:
+  let
+    id = topic["id"].getInt()
+    raw_post = client.getContent(forum_url &  "/raw/" & intToStr(id))
+    slug = topic["slug"].getStr()
+    title = topic["title"].getStr()
+    page = front_matter.format(title, slug) & raw_post
+
+  writeFile("_pages/" & slug & ".md", page)
+  echo "Saving page: \"" & title & "\""
+
+  # Download images used in posts
   for image in findAll(raw_post, re"<img[^>]+src='([^'>]+)'"):
-    var src = image[11..^2]
-    echo src
+    let src = image[11..^2]
+    echo "Downloading image: " & src
     client.downloadFile("https://forums.freeside.co.uk/" & src, src)
